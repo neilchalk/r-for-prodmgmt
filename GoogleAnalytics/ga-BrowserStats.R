7185526b50158a1f5c3bf1c9698fb7fee988339c
Neil Chalk <neil.chalk@gmail.com>
2017-12-05 11:48:22 +0000

    Use factors for browser and country names
    If only because it makes the summary stats more useful

--- GoogleAnalytics/ga-BrowserStats.R
+++ GoogleAnalytics/ga-BrowserStats.R
@@ -3,10 +3,18 @@ library(dplyr)
 library(readr)
 
 
-sessions <- read_csv("data/sessions_browser_stats.csv")
-
 #set the number of sessions that have a screen size before we are interested in them
 session_cutoff <- 100
+#set factors where we are fairly sure we know the values in advance
+browserTypes <- col_factor(levels = c("Chrome", "Edge", "Firefox", "Internet Explorer", "Opera", "Safari"))
+int <- col_integer()
+char <- col_character()
+
+#read in data
+sessions <- read_csv("data/sessions_browser_stats.csv", col_types = list(int, browserTypes, char, char, char, char, int))
+#different way to set factors, using data imported
+sessions$country <- factor(sessions$country, levels = unique(sessions$country)) 
+
 
 # Now find the aggregates for different screen sizes to get the subset we are interested in
 session_aggregates <- aggregate(sessions$hits,list(screenResolution = sessions$screenResolution), sum)
