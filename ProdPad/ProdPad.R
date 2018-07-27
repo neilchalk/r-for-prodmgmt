ce52a72d6d4617404af5d18d395df7b11cd7e1b4
Neil Chalk <neil.chalk@gmail.com>
2018-07-27 16:32:07 +0100

    Add in workflow state of ideas from secondary call

--- ProdPad/ProdPad.R
+++ ProdPad/ProdPad.R
@@ -1,10 +1,12 @@
 library(jsonlite)
 
-baseurl <- "https://api.prodpad.com/v1/ideas?apikey=1"
+apikey <- "1"
+baseurl <- "https://api.prodpad.com/v1/ideas?apikey="
+
 pages <- list()
 #TODO add in paging logic based on initial response
 for(i in 0:9){
-  mydata <- fromJSON(paste0(baseurl, "&page=", i), flatten=TRUE)
+  mydata <- fromJSON(paste0(baseurl, apikey, "&page=", i), flatten=TRUE)
   message("Retrieving page ", i)
   pages[[i+1]] <- mydata$ideas
 }
@@ -20,6 +22,24 @@ backlog$effort <- factor(backlog$effort)
 backlog$account.id  <- NULL  
 backlog$account.slug <- NULL       
 backlog$account.name <- NULL
+
+i <- 1
+WIP <- vector()
+
+for(idea in backlog$id){
+  mydata <- fromJSON(paste0("https://api.prodpad.com/v1/ideas/", idea, "?apikey=", apikey,"&expand"), flatten=TRUE)
+ # message("Retrieving page ", i)
+  
+  if (!is.null(mydata$status)) { 
+    WIP[[i]] <- mydata$status$status
+    
+  } else {
+    WIP[[i]] <- "" 
+  }
+  i <- i + 1
+}
+
+backlog$workflow.status <- factor(WIP)
 
 #check output
 summary(backlog)
--- ProdPad/README.md
+++ ProdPad/README.md
@@ -6,7 +6,7 @@ Work in progress to get data from the live ProdPad system and report on the stat
 
 ``` r
 
-baseurl <- "https://api.prodpad.com/v1/ideas?apikey=1"
+apikey <- "1"
 ```
 
 You need to add your [API key](https://help.prodpad.com/hc/en-us/articles/204956707-How-do-I-generate-an-API-key-) for your ProdPad user and account, in place of the value "1" in the script by default.
