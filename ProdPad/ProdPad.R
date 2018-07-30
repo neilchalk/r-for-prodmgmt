49340b4f08264ac410428429c796997f0391c2f8
Neil Chalk <neil.chalk@gmail.com>
2018-07-30 16:51:59 +0100

    started to extract functions

--- ProdPad/ProdPad.R
+++ ProdPad/ProdPad.R
@@ -4,6 +4,54 @@ library(dplyr)
 apikey <- "1"
 baseurl <- "https://api.prodpad.com/v1/ideas?apikey="
 
+tag_status <- function(backlog){
+  # Get extended details to group ideas by goal
+  i <- 1
+  isEngagement <- vector()
+  isRevenue <- vector()
+  isUsability <- vector()
+  WIP <- vector()
+
+  for(idea in backlog$id){
+    mydata <- fromJSON(paste0("https://api.prodpad.com/v1/ideas/", idea, "?apikey=", apikey,"&expand"), flatten=TRUE)
+
+    if (!is.null(mydata$status)) { 
+      WIP[[i]] <- mydata$status$status
+    } else {
+      WIP[[i]] <- "New Idea" 
+    }
+    
+    if (!is.null(mydata$tags)) { 
+      for(tag in  mydata$tags$tag){
+        isEngagement[[i]] <- 0
+        isRevenue[[i]] <- 0
+        isUsability[[i]] <- 0
+        switch(tag,
+               "User Engagement" = isEngagement[[i]] <- 1,
+               "Portal Usability" = isUsability[[i]] <- 1,
+               "Revenue Generation" = isRevenue[[i]] <- 1)
+      } 
+    } else {
+      isEngagement[[i]] <- 0
+      isRevenue[[i]] <- 0
+      isUsability[[i]] <- 0
+    }
+    
+    i <- i + 1
+  }
+
+  isEngagement[is.na(isEngagement)] <- 0
+  isRevenue[is.na(isRevenue)] <- 0
+  isUsability[is.na(isUsability)] <- 0
+
+  backlog$isGoal1 <- isEngagement
+  backlog$isGoal2 <- isRevenue
+  backlog$isGoal3 <- isUsability
+  backlog$workflow.status <- WIP
+  return(backlog)
+}
+
+##### START OF DATA RETRIEVAL ########
 
 pages <- list()
 #TODO add in paging logic based on initial response
@@ -16,52 +64,8 @@ for(i in 0:9){
 #combine all into one
 backlog <- rbind_pages(pages)
 
-# Get extended details to group ideas by goal
-i <- 1
-isEngagement <- vector()
-isRevenue <- vector()
-isUsability <- vector()
-WIP <- vector()
-
-for(idea in backlog$id){
-  mydata <- fromJSON(paste0("https://api.prodpad.com/v1/ideas/", idea, "?apikey=", apikey,"&expand"), flatten=TRUE)
-  
-  if (!is.null(mydata$status)) { 
-    WIP[[i]] <- mydata$status$status
-    
-  } else {
-    WIP[[i]] <- "New Idea" 
-  }
-  if (!is.null(mydata$tags)) { 
-    for(tag in  mydata$tags$tag){
-      isEngagement[[i]] <- 0
-      isRevenue[[i]] <- 0
-      isUsability[[i]] <- 0
-      switch(tag,
-             "User Engagement" = isEngagement[[i]] <- 1,
-             "Portal Usability" = isUsability[[i]] <- 1,
-             "Revenue Generation" = isRevenue[[i]] <- 1)
-    } 
-    
-  } else {
-    isEngagement[[i]] <- 0
-    isRevenue[[i]] <- 0
-    isUsability[[i]] <- 0
-  }
-  
-  i <- i + 1
-}
-
-isEngagement[is.na(isEngagement)] <- 0
-isRevenue[is.na(isRevenue)] <- 0
-isUsability[is.na(isUsability)] <- 0
-
-backlog$isGoal1 <- isEngagement
-backlog$isGoal2 <- isRevenue
-backlog$isGoal3 <- isUsability
-backlog$workflow.status <- WIP
-
 backlog %>% 
+  tag_status() %>%
   distinct(id, .keep_all = TRUE) %>%
   select(-.data$account.id, -.data$account.slug, -.data$account.name, 
          -.data$creator.id, -.data$creator.username) %>%
