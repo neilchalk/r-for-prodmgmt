e674b7c798eeb205efdd035c4cf6c05357bd727a
Neil Chalk <neil.chalk@gmail.com>
2018-08-31 14:16:24 +0100

    Calculate the raw prioritisation score of ideas

--- ProdPad/ProdPad.R
+++ ProdPad/ProdPad.R
@@ -4,9 +4,14 @@ library(dplyr)
 apikey <- "1"
 baseurl <- "https://api.prodpad.com/v1/ideas?apikey="
 
+#change the line below to make sure that you are in the correct workign directory
+#setwd("C:/Temp/R/ProdPad")
 source("ProdPad-functions.R")
 
+##### START OF DATA RETRIEVAL ########
 backlog <- retrieve_ideas()
+# set the raw prioritisation score 
+backlog$raw <- backlog$impact %/0% backlog$effort
 
 backlog %>% 
   tag_status() %>%
